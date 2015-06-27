60e7f58f905709df26b9c85f30a5751b6672d6e3
wizard23 <wizards23@gmail.com>
2015-06-27 22:02:32 +0200

    working on espTerm.py now with 2 windows and serial reset on start :)

--- Firmware/espTerm.py
+++ Firmware/espTerm.py
@@ -1,29 +1,46 @@
 import serial
 import curses
+import time
+
 
 def main(stdscr):
+
+    #curses.noecho()
+    #curses.cbreak()
+
     # do not wait for input when calling getch
     stdscr.nodelay(1)
 
+    # stdscr.keypad(1)
+
+    begin_x = 20; begin_y = 7
+    height = 20; width = 60
+    win = curses.newwin(height, width, begin_y, begin_x)
+    win.clear()
 
     port = serial.Serial("/dev/ttyUSB0", 115200, timeout=0)
+    port.setDTR(False)
+    port.setRTS(True)
+    time.sleep(0.05)
+    port.setRTS(False)
 
+    xx = 0
     refresh = False
     while True:
         s = port.read(1)
         if (len(s) > 0):
-            stdscr.clear()
-            stdscr.addstr("serial: ")
-            stdscr.addstr(str(ord(s)))
-            stdscr.addstr(" : ")
-            refresh = True
+            if (xx == 0):
+                win.clear()
+            xx = (xx + 1) % 250
+            win.addstr(str(ord(s)))
+            win.addstr("/")
+            win.refresh()
 
         # get keyboard input, returns -1 if none available
         c = stdscr.getch()
         if c != -1:
-            # print numeric value
             stdscr.clear()
-            stdscr.addstr("keyboard: ")
+            stdscr.addstr("key: ")
             stdscr.addstr(str(c))
             refresh = True;
             stdscr.addstr(" ")
@@ -39,11 +56,7 @@ def main(stdscr):
             if c == 68: # D
                  stdscr.addstr("DTR <- False")
                  port.setDTR(False)
-
-
-        if refresh:
             stdscr.refresh()
-            # return curser to start position
             stdscr.move(0, 0)
 
 if __name__ == '__main__':
