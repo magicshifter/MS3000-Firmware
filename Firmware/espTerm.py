ed6ae21eb3a925dd598f0929c5979c7e2c3a6428
wizard23 <wizards23@gmail.com>
2015-06-27 22:10:43 +0200

    hacking pon espTerm.py output of serial and keyboard data

--- Firmware/espTerm.py
+++ Firmware/espTerm.py
@@ -13,10 +13,10 @@ def main(stdscr):
 
     # stdscr.keypad(1)
 
-    begin_x = 20; begin_y = 7
-    height = 20; width = 60
-    win = curses.newwin(height, width, begin_y, begin_x)
-    win.clear()
+    begin_x = 20; begin_y = 40
+    height = 40; width = 80
+    keyboardWin = curses.newwin(height, width, begin_y, begin_x)
+    keyboardWin.clear()
 
     port = serial.Serial("/dev/ttyUSB0", 115200, timeout=0)
     port.setDTR(False)
@@ -30,34 +30,31 @@ def main(stdscr):
         s = port.read(1)
         if (len(s) > 0):
             if (xx == 0):
-                win.clear()
-            xx = (xx + 1) % 250
-            win.addstr(str(ord(s)))
-            win.addstr("/")
-            win.refresh()
+                stdscr.clear()
+            xx = (xx + 1) % 2550
+            if ord(s) >= 32 and ord(s) <= 127:
+                stdscr.addstr(s)
+            else:
+                stdscr.addstr('?')
+            stdscr.refresh()
 
         # get keyboard input, returns -1 if none available
         c = stdscr.getch()
         if c != -1:
-            stdscr.clear()
-            stdscr.addstr("key: ")
-            stdscr.addstr(str(c))
-            refresh = True;
-            stdscr.addstr(" ")
+            keyboardWin.addstr(chr(c))
             if c == 114: # r
-                stdscr.addstr("RTS <- True")
+                keyboardWin.addstr("RTS <- True")
                 port.setRTS(True)
             if c == 82: # R
-                 stdscr.addstr("RTS <- False")
+                 keyboardWin.addstr("RTS <- False")
                  port.setRTS(False)
             if c == 100: # d
-                stdscr.addstr("DTR <- True")
+                keyboardWin.addstr("DTR <- True")
                 port.setDTR(True)
             if c == 68: # D
-                 stdscr.addstr("DTR <- False")
+                 keyboardWin.addstr("DTR <- False")
                  port.setDTR(False)
-            stdscr.refresh()
-            stdscr.move(0, 0)
+            keyboardWin.refresh()
 
 if __name__ == '__main__':
     curses.wrapper(main)
