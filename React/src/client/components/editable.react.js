8411313d97c20a749292555f17ebd512eda00c07
Jascha Ehrenreich <jascha@jaeh.at>
2015-07-28 01:49:03 +0200

    moved proptypes and defaultprops in editable class

--- React/src/client/components/editable.react.js
+++ React/src/client/components/editable.react.js
@@ -15,6 +15,40 @@ const initialState = new State;
 
 export default class Editable extends Component {
 
+  static propTypes = {
+    className: React.PropTypes.string,
+    disabled: React.PropTypes.bool,
+    editButtons: React.PropTypes.func,
+    id: React.PropTypes.oneOfType([React.PropTypes.number, React.PropTypes.string]).isRequired,
+    isRequired: React.PropTypes.bool,
+    maxRows: React.PropTypes.oneOfType([React.PropTypes.string, React.PropTypes.number]),
+    name: React.PropTypes.string.isRequired,
+    onSave: React.PropTypes.func.isRequired,
+    onState: React.PropTypes.func.isRequired,
+    rows: React.PropTypes.oneOfType([React.PropTypes.string, React.PropTypes.number]),
+    showEditButtons: React.PropTypes.bool,
+    showViewButtons: React.PropTypes.bool,
+    state: React.PropTypes.instanceOf(State),
+    text: React.PropTypes.string.isRequired,
+    type: React.PropTypes.string,
+    viewButtons: React.PropTypes.func,
+  }
+
+  static defaultProps = {
+    isRequired: true,
+    showEditButtons: false,
+    showViewButtons: false,
+    editButtons: (onSaveClick, onCancelClick, disabled) =>
+      <div className="btn-group">
+        <button disabled={disabled} onClick={onSaveClick}>Save</button>
+        <button disabled={disabled} onClick={onCancelClick}>Cancel</button>
+      </div>,
+    viewButtons: (onEditClick, disabled) =>
+      <div className="btn-group">
+        <button disabled={disabled} onClick={onEditClick}>Edit</button>
+      </div>,
+  }
+
   constructor(props) {
     super(props);
     this.cancelEdit = this.cancelEdit.bind(this);
@@ -147,37 +181,3 @@ export default class Editable extends Component {
   }
 
 }
-
-Editable.propTypes = {
-  className: React.PropTypes.string,
-  disabled: React.PropTypes.bool,
-  editButtons: React.PropTypes.func,
-  id: React.PropTypes.oneOfType([React.PropTypes.number, React.PropTypes.string]).isRequired,
-  isRequired: React.PropTypes.bool,
-  maxRows: React.PropTypes.oneOfType([React.PropTypes.string, React.PropTypes.number]),
-  name: React.PropTypes.string.isRequired,
-  onSave: React.PropTypes.func.isRequired,
-  onState: React.PropTypes.func.isRequired,
-  rows: React.PropTypes.oneOfType([React.PropTypes.string, React.PropTypes.number]),
-  showEditButtons: React.PropTypes.bool,
-  showViewButtons: React.PropTypes.bool,
-  state: React.PropTypes.instanceOf(State),
-  text: React.PropTypes.string.isRequired,
-  type: React.PropTypes.string,
-  viewButtons: React.PropTypes.func,
-};
-
-Editable.defaultProps = {
-  isRequired: true,
-  showEditButtons: false,
-  showViewButtons: false,
-  editButtons: (onSaveClick, onCancelClick, disabled) =>
-    <div className="btn-group">
-      <button disabled={disabled} onClick={onSaveClick}>Save</button>
-      <button disabled={disabled} onClick={onCancelClick}>Cancel</button>
-    </div>,
-  viewButtons: (onEditClick, disabled) =>
-    <div className="btn-group">
-      <button disabled={disabled} onClick={onEditClick}>Edit</button>
-    </div>,
-};
