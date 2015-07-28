c7efc75ea6b0ec008552bf39e7afb764939c5a28
Jascha Ehrenreich <jascha@jaeh.at>
2015-07-28 13:13:13 +0200

    added form styles

---
+++ React/src/client/styles/form.js
@@ -0,0 +1,32 @@
+export default {
+  form: {
+    width: '100%',
+  },
+  input: {
+    fontSize: '1.2em',
+    borderBottom: '1px solid #B0BEC5',
+    borderRight: '3px solid #607D8B',
+    color: '#607D8B',
+    padding: '.4em .4em 0 .4em',
+    backgroundColor: '#ECEFF1',
+    borderRadius: '.3em',
+  },
+  textarea: {
+    fontSize: '1.2em',
+    borderBottom: '1px solid #B0BEC5',
+    borderRight: '3px solid #607D8B',
+    color: '#607D8B',
+    padding: '.4em .4em 0 .4em',
+    backgroundColor: '#ECEFF1',
+    borderRadius: '.3em',
+  },
+  button: {
+
+  },
+  checkbox: {
+
+  },
+  errorMessage: {
+    color: 'red',
+  },
+};
