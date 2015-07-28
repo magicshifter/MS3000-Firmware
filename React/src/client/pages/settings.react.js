98648aad2cee32d3349dafb42de0c4bdd00b292a
Jascha Ehrenreich <jascha@jaeh.at>
2015-07-28 17:57:48 +0200

    adapt markup to use article tag

--- React/src/client/pages/settings.react.js
+++ React/src/client/pages/settings.react.js
@@ -4,17 +4,18 @@ import React from 'react';
 import requireAuth from '../auth/requireauth.react';
 import {msg} from '../intl/store';
 import ServerForm from '../settings/server/server.react';
-import AccesspointForm from '../settings/accesspoint/accesspoint.react';
+import AccessPointForm from '../settings/accesspoint/accesspoint.react';
 import Page from '../components/page.react';
 
 class Settings extends Component {
 
   render() {
-    let append = <ServerForm {...this.props} />;
-    append += <AccesspointForm {...this.props} />;
-
     return (
-      <Page page='settings' append={append}></Page>
+      <article className='settings'>
+        <Page page='settings' />
+        <ServerForm {...this.props} />
+        <AccessPointForm {...this.props} />
+      </article>
     );
   }
 
