eeb91dca8b9176b0143e14f7a9e42315624086ab
Jascha Ehrenreich <jascha@jaeh.at>
2015-07-28 17:59:50 +0200

    change markup and add global styles

--- React/src/client/settings/accesspoint/accesspoint.react.js
+++ React/src/client/settings/accesspoint/accesspoint.react.js
@@ -6,6 +6,8 @@ import immutable from 'immutable';
 import {focusInvalidField} from '../../lib/validation';
 import {msg} from '../../intl/store';
 
+import styles from '../../styles/form.js';
+
 export default class AccesspointForm extends Component {
 
   static propTypes = {
@@ -30,29 +32,45 @@ export default class AccesspointForm extends Component {
 
     return (
       <div className='settings-accesspoint'>
-        <form onSubmit={(e) => this.onFormSubmit(e)}>
+        <form onSubmit={(e) => this.onFormSubmit(e)} style={styles.form}>
           <fieldset disabled={pendingActions.has(actions.saveAccesspoint.toString())}>
-            <legend>{msg('forms.settings.accesspoint.legend')}</legend>
-            <input
-              name='ssid'
-              onChange={actions.updateFormField}
-              placeholder={msg('forms.settings.accesspoint.placeholder.ssid')}
-              value={form.fields.ssid}
-            />
-            <br />
-            <input
-              name='password'
-              onChange={actions.updateFormField}
-              placeholder={msg('forms.settings.accesspoint.placeholder.password')}
-              type='password'
-              value={form.fields.password}
-            />
-            <button
-              children={msg('buttons.save')}
-              type='submit'
-            />
+            <legend>
+              <h2>{msg('forms.settings.accesspoint.legend')}</h2>
+            </legend>
+            <div style={styles.field}>
+              <label forHtml='ssid'>
+                {msg('forms.settings.accesspoint.label.ssid')}
+              </label>
+              <input
+                name='ssid'
+                style={styles.input}
+                onChange={actions.updateFormField}
+                placeholder={msg('forms.settings.accesspoint.placeholder.ssid')}
+                value={form.fields.ssid}
+              />
+            </div>
+            <div style={styles.field}>
+              <label forHtml='password'>
+                {msg('forms.settings.accesspoint.label.password')}
+              </label>
+              <input
+                name='password'
+                style={styles.input}
+                onChange={actions.updateFormField}
+                placeholder={msg('forms.settings.accesspoint.placeholder.password')}
+                type='password'
+                value={form.fields.password}
+              />
+            </div>
+            <div style={styles.buttonContainer}>
+              <button
+                style={styles.button}
+                children={msg('buttons.save')}
+                type='submit'
+              />
+            </div>
             {form.error &&
-              <span className='error-message'>{form.error.message}</span>
+              <span style={styles.errorMessage}>{form.error.message}</span>
             }
           </fieldset>
         </form>
--- React/src/client/settings/server/server.react.js
+++ React/src/client/settings/server/server.react.js
@@ -6,6 +6,8 @@ import immutable from 'immutable';
 import {focusInvalidField} from '../../lib/validation';
 import {msg} from '../../intl/store';
 
+import styles from '../../styles/form';
+
 export default class ServerForm extends Component {
 
   static propTypes = {
@@ -30,31 +32,46 @@ export default class ServerForm extends Component {
 
     return (
       <div className='settings-server'>
-        <form onSubmit={(e) => this.onFormSubmit(e)}>
-          <fieldset disabled={pendingActions.has(actions.saveServer.toString())}>
-            <legend>{msg('forms.settings.server.legend')}</legend>
-            <input
-              autoFocus
-              name='hostname'
-              onChange={actions.updateFormField}
-              placeholder={msg('forms.settings.server.placeholder.hostname')}
-              value={form.fields.hostname}
-            />
-            <br />
-            <input
-              name='port'
-              onChange={actions.updateFormField}
-              placeholder={msg('forms.settings.server.placeholder.port')}
-              type='number'
-              value={form.fields.port}
-            />
-            <button
-              children={msg('buttons.save')}
-              type='submit'
-            />
-            {form.error &&
-              <span className='error-message'>{form.error.message}</span>
-            }
+        <form onSubmit={(e) => this.onFormSubmit(e)} style={styles.form}>
+          <fieldset
+            disabled={pendingActions.has(actions.saveServer.toString())}
+          >
+            <legend><h2>{msg('forms.settings.server.legend')}</h2></legend>
+            <div style={styles.field}>
+              <label htmlFor='hostname'>
+                {msg('forms.settings.server.label.hostname')}
+              </label>
+              <input
+                name='hostname'
+                autoFocus
+                style={styles.input}
+                onChange={actions.updateFormField}
+                placeholder={msg('forms.settings.server.placeholder.hostname')}
+                value={form.fields.hostname}
+              />
+            </div>
+            <div style={styles.field}>
+              <label htmlFor='port'>
+                {msg('forms.settings.server.label.port')}
+              </label>
+              <input
+                name='port'
+                style={styles.input}
+                onChange={actions.updateFormField}
+                placeholder={msg('forms.settings.server.placeholder.port')}
+                type='number'
+                value={form.fields.port}
+              />
+            </div>
+            <div style={styles.buttonContainer}>
+              <button
+                children={msg('buttons.save')}
+                type='submit'
+              />
+              {form.error &&
+                <span className='error-message'>{form.error.message}</span>
+              }
+            </div>
           </fieldset>
         </form>
       </div>
