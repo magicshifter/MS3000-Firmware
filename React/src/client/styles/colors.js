d19bbc69977718def7f6a62edb67c67247652bf1
Jascha Ehrenreich <jascha@jaeh.at>
2015-07-28 17:57:03 +0200

    add additional form styles and colors

---
+++ React/src/client/styles/colors.js
@@ -0,0 +1,17 @@
+export default {
+  input: {
+    backgroundColor: '#ECEFF1',
+    color: '#607D8B',
+    borderRight: '#607D8B',
+    borderBottom: '#B0BEC5',
+  },
+  button: {
+    backgroundColor: '#607D8B',
+    color: '#ECEFF1',
+    borderRight: '#ECEFF1',
+    borderBottom: '#ECEFF1',
+  },
+  errorMessage: {
+    color: 'red',
+  },
+};
--- React/src/client/styles/form.js
+++ React/src/client/styles/form.js
@@ -1,32 +1,55 @@
+import {input, button, errorMessage} from './colors';
+
 export default {
   form: {
     width: '100%',
   },
+  field: {
+    textAlign: 'right',
+    maxWidth: '50%',
+    margin: '0 auto 1em',
+  },
   input: {
     fontSize: '1.2em',
-    borderBottom: '1px solid #B0BEC5',
-    borderRight: '3px solid #607D8B',
-    color: '#607D8B',
+    borderBottom: `1px solid ${input.borderBottom}`,
+    borderRight: `3px solid ${input.borderRight}`,
+    color: input.color,
     padding: '.4em .4em 0 .4em',
-    backgroundColor: '#ECEFF1',
+    backgroundColor: input.backgroundColor,
     borderRadius: '.3em',
+    '-webkit-box-shadow': `0 0 0 50em ${input.backgroundColor} inset`,
+    maxWidth: '70%',
   },
   textarea: {
     fontSize: '1.2em',
-    borderBottom: '1px solid #B0BEC5',
-    borderRight: '3px solid #607D8B',
-    color: '#607D8B',
+    borderBottom: '1px solid ${input.borderBottom}',
+    borderRight: '3px solid ${input.borderRight}',
+    color: input.color,
     padding: '.4em .4em 0 .4em',
-    backgroundColor: '#ECEFF1',
+    backgroundColor: input.backgroundColor,
     borderRadius: '.3em',
+    WebkitBoxShadow: `0 0 0 50em ${input.backgroundColor} inset`,
+  },
+  buttonContainer: {
+    maxWidth: '50%',
+    textAlign: 'right',
+    margin: '0 auto',
   },
   button: {
-
+    fontSize: '1.2em',
+    borderBottom: `1px solid ${button.borderBottom}`,
+    borderRight: `3px solid ${button.borderRight}`,
+    borderTop: 0,
+    borderLeft: 0,
+    color: button.color,
+    padding: '.4em .4em 0 .4em',
+    backgroundColor: button.backgroundColor,
+    borderRadius: '.3em',
   },
   checkbox: {
 
   },
   errorMessage: {
-    color: 'red',
+    color: errorMessage.color,
   },
 };
