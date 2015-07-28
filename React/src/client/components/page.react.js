d8e4e20b4f85c76d05a3e0676fe8286212711535
Jascha Ehrenreich <jascha@jaeh.at>
2015-07-28 03:48:28 +0200

    added documenttitle to page.react

--- React/src/client/components/page.react.js
+++ React/src/client/components/page.react.js
@@ -2,6 +2,7 @@ import Component from '../components/component.react';
 import React from 'react';
 import {FormattedHTMLMessage} from 'react-intl';
 import {msg} from '../intl/store';
+import DocumentTitle from 'react-document-title';
 
 import Title from './page/title.react';
 import Content from './page/content.react';
@@ -9,11 +10,23 @@ import Content from './page/content.react';
 export default class Page extends Component {
 
   render() {
+    let title = '';
+
+    try {
+      title = msg(`pages.${this.props.page}.title`);
+    }
+    catch (e) {
+      console.warn(e);
+      title = '';
+    }
+
     return (
-      <div className={`${this.props.page} page`}>
-        <Title {...this.props}></Title>
-        <Content {...this.props}></Content>
-      </div>
+      <DocumentTitle title={title}>
+        <div>
+          <Title {...this.props}></Title>
+          <Content {...this.props}></Content>
+        </div>
+      </DocumentTitle>
     );
   }
 }
