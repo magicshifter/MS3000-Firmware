1ea8cfbb5b52fea6f16baf8adf3fef8b48c20e30
Jascha Ehrenreich <jascha@jaeh.at>
2015-07-28 19:57:56 +0200

    change routes, remove home

--- React/src/client/routes.js
+++ React/src/client/routes.js
@@ -1,5 +1,4 @@
 import App from './app/app.react';
-import Home from './pages/home.react';
 import Login from './pages/login.react';
 import Settings from './pages/settings.react';
 import Leds from './pages/leds.react';
@@ -10,10 +9,9 @@ import {DefaultRoute, NotFoundRoute, Route} from 'react-router';
 
 export default (
   <Route handler={App} path='/'>
-    <DefaultRoute handler={Home} name='home' />
-    <Route handler={Leds} name='leds' />
-    <Route handler={Draw} name='draw' />
+    <DefaultRoute handler={Leds} name='leds' />
     <Route handler={Login} name='login' />
+    <Route handler={Draw} name='draw' />
     <Route handler={Settings} name='settings' />
     <NotFoundRoute handler={NotFound} name='not-found' />r
   </Route>
