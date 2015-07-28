e3b9426d62a5c43cfb06ef8fddc28fcc4a76748e
Jascha Ehrenreich <jascha@jaeh.at>
2015-07-28 19:45:06 +0200

    change header markup and css

--- React/src/client/app/header.react.js
+++ React/src/client/app/header.react.js
@@ -4,10 +4,11 @@ import {FormattedHTMLMessage} from 'react-intl';
 import {Link} from 'react-router';
 import {msg} from '../intl/store';
 import Logout from '../auth/logout.react';
-import styles from './app.styles';
 import Logo from '../components/logo.react';
 import Nav from '../components/nav.react';
 import StatusBar from '../components/statusbar.react';
+
+import styles from './app.styles';
 
 class Header extends Component {
 
@@ -26,20 +27,24 @@ class Header extends Component {
       menuItems.push('login');
     }
 
+    let homeLinksTo = 'login';
     if (isLoggedIn) {
       menuItems.push(<Logout key='logout' />);
+      homeLinksTo = 'settings';
     }
 
     return (
-      <header className='main' style={styles.header.container}>
+      <header className='main' style={styles.header.wrapper}>
         <div style={styles.header.background}></div>
-        <Link to='home' style={styles.header.link}>
-          <Logo src='logo.png' size='10vw' float='left' />
-          <h1 style={styles.header.h1}>
-            <FormattedHTMLMessage message={msg('header.h1Html')} />
-          </h1>
-        </Link>
-        <Nav menuItems={menuItems} />
+        <div style={styles.header.container}>
+          <Link to={homeLinksTo} style={styles.header.link}>
+            <Logo src='logo.png' size='10vw' float='left' />
+            <h1 style={styles.header.h1}>
+              <FormattedHTMLMessage message={msg('header.h1Html')} />
+            </h1>
+          </Link>
+          <Nav menuItems={menuItems} />
+        </div>
         <StatusBar {...this.props} />
       </header>
     );
