e14e3037f6cd680d5d4c7ea5652fad6bd478c7c3
Jascha Ehrenreich <jascha@jaeh.at>
2015-07-29 20:35:43 +0200

    renamed accesspointSettingsCursor to apSettingsCursor

--- React/src/client/settings/accesspoint/store.js
+++ React/src/client/settings/accesspoint/store.js
@@ -1,19 +1,19 @@
 import * as actions from './actions';
-import {accesspointSettingsCursor} from '../../state';
+import {apSettingsCursor} from '../../state';
 import {register} from '../../dispatcher';
 
 export const dispatchToken = register(({action, data}) => {
 
   switch (action) {
     case actions.saveFormError:
-      accesspointSettingsCursor(ap => {
+      apSettingsCursor(ap => {
         const error = data;
         return ap.setIn(['form', 'error'], error);
       });
       break;
 
     case actions.updateFormField:
-      accesspointSettingsCursor(ap => {
+      apSettingsCursor(ap => {
         const {name, value} = data;
         return ap.setIn(['form', 'fields', name], value);
       });
