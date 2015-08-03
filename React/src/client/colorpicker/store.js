fa39d7fe57f79aa0fdd7107f0429b490d0cb434f
Jascha Ehrenreich <jascha@jaeh.at>
2015-08-03 16:41:05 +0200

    fixed cursor variable name

--- React/src/client/colorpicker/store.js
+++ React/src/client/colorpicker/store.js
@@ -8,26 +8,24 @@ export const dispatchToken = register(({action, data}) => {
   switch (action) {
     case actions.addColor:
       colorPickerCursor(cursor => {
-        let colors = settings.get('colors');
+        let colors = cursor.get('colors');
         if (colors.indexOf(data) < 0) {
           colors = colors.push(data);
         }
 
-        return settings.set('colors', colors);
+        return cursor.set('colors', colors);
       });
 
       break;
 
     case actions.removeColor:
       colorPickerCursor(cursor => {
-        let colors = settings
+        let colors = cursor
                       .get('colors')
                       .filter((val) => val !== data);
-        return settings.set('colors', colors);
+        return cursor.set('colors', colors);
       });
 
       break;
-
   }
-
 });
