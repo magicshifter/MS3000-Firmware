8ea22af350566a56ffe58b78333fc0bf31cb4732
Jascha Ehrenreich <jascha@jaeh.at>
2015-08-03 22:01:04 +0200

    add styles

--- React/src/client/leds/ledsmenu.react.js
+++ React/src/client/leds/ledsmenu.react.js
@@ -7,21 +7,37 @@ import * as actions from './actions';
 export default class LedsMenu extends Component {
 
   render() {
+    const styles = {
+      container: {
+
+      },
+      lh: {
+        float: 'left',
+        margin: '0 5px 0',
+      },
+      li: {
+        float: 'left',
+        margin: '0 5px 0 0',
+
+      },
+    };
+
+
     return (
       <div>
-        <h3>{msg('pages.leds.menu.title')}</h3>
         <ul>
-          <li>
+          <lh style={styles.lh}>{msg('pages.leds.menu.title')}</lh>
+          <li style={styles.li}>
             <button onClick={(e) => actions.selectAllLeds(e)}>
               {msg('pages.leds.menu.all')}
             </button>
           </li>
-          <li>
+          <li style={styles.li}>
             <button onClick={(e) => actions.toggleAllLeds(e)}>
               {msg('pages.leds.menu.toggle')}
             </button>
           </li>
-          <li>
+          <li style={styles.li}>
             <button onClick={(e) => actions.deselectAllLeds(e)}>
               {msg('pages.leds.menu.none')}
             </button>
