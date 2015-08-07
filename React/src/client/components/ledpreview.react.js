cd65e979008c8d0cabf784ceaae5729f08c4fa1a
Jascha Ehrenreich <jascha@jaeh.at>
2015-08-07 23:51:26 +0200

    fix css, remove unneeded stuff

--- React/src/client/components/ledpreview.react.js
+++ React/src/client/components/ledpreview.react.js
@@ -3,41 +3,24 @@ import React from 'react';
 import Component from '../components/component.react';
 import {msg} from '../intl/store';
 
-import * as actions from '../leds/actions';
-
 import merge from 'magic-merge';
 
 export default class LedPreview extends Component {
 
-  onClick(e) {
-    e.preventDefault();
-    actions.toggleLed(this.props);
-  }
-
   render() {
+    const {value, size} = this.props;
+
     const styles = {
       container: merge({
         display: 'inline-block',
-        height: '100%',
-        width: '100%',
-        backgroundColor: '#000000',
+        backgroundColor: value || '#000000',
+        width: size ? size : '100%',
+        height: size ? size : '100%',
       }, this.props.style),
     };
 
-    const {value, size} = this.props;
-    let containerStyle = styles.container;
-
-    if (size) {
-      containerStyle.width = size;
-      containerStyle.height = size;
-    }
-
-    if (value) {
-      containerStyle.backgroundColor = value;
-    }
-
     return (
-      <div style={containerStyle}></div>
+      <a style={styles.container}></a>
     );
   }
 }
