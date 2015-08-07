aea020a10a351b9b9e83c5f7cc75688e1bd2603e
Jascha Ehrenreich <jascha@jaeh.at>
2015-08-07 23:48:41 +0200

    clean up led css, remove unneeded stuff

--- React/src/client/leds/led.react.js
+++ React/src/client/leds/led.react.js
@@ -4,40 +4,34 @@ import * as actions from './actions';
 import Component from '../components/component.react';
 import LedPreview from '../components/ledpreview.react';
 
+import merge from 'magic-merge';
+
 export default class Led extends Component {
 
   render() {
+    const { value, active, ledId, style } = this.props;
+
     const styles = {
-      led: {
+      led: merge({
         float: 'left',
         height: '4.9vw',
         width: '4.9vw',
         display: 'block',
         border: '.5vw solid',
-        borderColor: 'grey',
+        borderColor: active ? 'lightgrey' : 'grey',
         margin: '.1vw',
         maxHeight: '50px',
         maxWidth: '50px',
-      },
-      active: {
-        borderColor: 'lightgrey',
-      },
+      }, style),
     };
-
-    const { value, active, ledId } = this.props;
-    if (active) {
-      styles.led.borderColor = styles.active.borderColor;
-    }
 
     return (
       <div
         style={styles.led}
         onMouseUp={e => actions.toggleLed({ledId, active})}
-        onDragEnter={e => actions.activateLed({e, ledId})}
       >
         <LedPreview
           value={value}
-          active={active}
         />
       </div>
     );
