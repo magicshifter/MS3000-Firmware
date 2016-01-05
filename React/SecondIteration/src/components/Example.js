05e88a65f964c800a45867c968f57a5630407bc9
Jascha Ehrenreich <jascha@jaeh.at>
2016-01-05 03:26:34 +0100

    es7ify component

--- React/SecondIteration/src/components/Example.js
+++ React/SecondIteration/src/components/Example.js
@@ -1,19 +1,45 @@
-import React, {Component} from 'react';
+// need to import react here
+import React, {Component, PropTypes} from 'react';
 
 export default class ExampleComponent extends Component {
+
+  static propTypes = {
+    text: PropTypes.string,
+  }
+
+  static defaultProps = {
+    text: "Example Component default text",
+  }
+
   constructor(props) {
     super(props);
-    this.props = {
-      text: 'Example Component',
+
+    this.state = {
+      text: props.text,
+      clicked: 0,
     };
   }
 
+  onButtonClick() {
+    const clicked = this.state.clicked + 1;
+    this.setState({
+      text: `You Clicked the Button ${this.state.clicked} times!`,
+      clicked,
+    });
+  }
+
   render() {
-    var text = this.props.text;
+    const {text} = this.state;
 
     return (
-      <div>{text}</div>
+      <div>
+        <p>{text}</p>
+        <button
+          onClick={this.onButtonClick.bind(this)}
+        >
+          Click Me
+        </button>
+      </div>
     );
   }
 }
-
