edf13c709a154fbd26adb3bb7eca4665dadef670
Jascha Ehrenreich <jascha@jaeh.at>
2016-01-10 21:12:56 +0100

    fix(bug) update routes

--- React/SecondIteration/src/routes/index.js
+++ React/SecondIteration/src/routes/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import { Route, IndexRoute, Redirect } from 'react-router';
 
-// NOTE: here we're making use of the `resolve.root` configuration
-// option in webpack, which allows us to specify import paths as if
-// they were from the root of the ~/src directory. This makes it
-// very easy to navigate to files regardless of how deeply nested
-// your current file is.
 import CoreLayout from 'layouts/CoreLayout';
+
 import AboutView from 'views/AboutView/AboutView';
 import PixelEditorView from 'views/PixelEditorView/PixelEditorView';
 import NotFoundView from 'views/NotFoundView/NotFoundView';
@@ -14,7 +10,7 @@ import NotFoundView from 'views/NotFoundView/NotFoundView';
 export default (
   <Route path='/' component={CoreLayout}>
     <IndexRoute component={PixelEditorView} />
-    <Route path='/pixels' component={AboutView} />
+    <Route path='/about' component={AboutView} />
     <Route path='/404' component={NotFoundView} />
     <Redirect from='*' to='/404' />
   </Route>
