23f9cd3a1427dca0b0c5bc83baac2a4aa772d554
Jascha Ehrenreich <jascha@jaeh.at>
2016-01-10 21:00:44 +0100

    update pixeleditorview

--- React/SecondIteration/src/views/PixelEditorView/PixelEditorView.js
+++ React/SecondIteration/src/views/PixelEditorView/PixelEditorView.js
@@ -7,83 +7,51 @@ import Pixel from './Pixel';
 
 import RGBAInput from 'components/inputs/RGBAInput';
 import ImageInput from 'components/inputs/ImageInput';
+import FileUploadInput from 'components/inputs/FileUploadInput';
 
 const mapStateToProps = (state) => {
-  return state.pixelEditor;
+  const {pixelEditor, settings, layout} = state;
+  return {
+    pixelEditor,
+    settings,
+    layout,
+  };
 };
 
 export class PixelEditorView extends Component {
   static propTypes = {
-    pixels: PropTypes.array.isRequired,
-    rows: PropTypes.number.isRequired,
-    columns: PropTypes.number.isRequired,
     pixelClick: PropTypes.func.isRequired,
     setColor: PropTypes.func.isRequired,
     setColorValue: PropTypes.func.isRequired,
-    color: PropTypes.shape({
-      r: PropTypes.number,
-      g: PropTypes.number,
-      b: PropTypes.number,
-      a: PropTypes.number,
+
+    pixelEditor: PropTypes.shape({
+      pixels: PropTypes.array.isRequired,
+      rows: PropTypes.number.isRequired,
+      columns: PropTypes.number.isRequired,
+
+      color: PropTypes.shape({
+        r: PropTypes.number,
+        g: PropTypes.number,
+        b: PropTypes.number,
+        a: PropTypes.number,
+      }).isRequired,
+    }).isRequired,
+
+    settings: PropTypes.shape({
+      protocol: PropTypes.string.isRequired,
+      host: PropTypes.string.isRequired,
+    }).isRequired,
+
+    layout: PropTypes.shape({
+      height: PropTypes.number.isRequired,
+      width: PropTypes.number.isRequired,
     }).isRequired,
   };
 
-  constructor(props) {
-    super(props);
-
-    this.renderPixel = this.renderPixel.bind(this);
-  }
-
-  renderPixel(pixel) {
-    const {row, column, color} = pixel;
-    const {columns, pixelClick} = this.props;
-
-    return (
-      <Pixel
-        key={`${column}-${row}`}
-        row={row}
-        column={column}
-        columns={columns}
-        color={color}
-        pixelClick={pixelClick}
-      />
-    );
-  }
-
-  handleUploadClick() {
-    var formData = new FormData();
-
-    // formData.append("username", "Groucho");
-    // formData.append("accountnum", 123456); // number 123456 is immediately converted to a string "123456"
-
-
-    var w = this.props.columns;
-    var h = this.props.rows;
-   
-    var fileSize = w*h*4;
-    var fileData = new Uint8Array(fileSize);
-    fileData[0] = 32;
-    fileData[1] = 1;
-    fileData[2] = 0xFF;
-
-    var blob = new Blob(fileData);
-
-    formData.append("uploadFile", blob);
-
-    var request = new XMLHttpRequest();
-    request.onload = function(oEvent) {
-      if (request.status == 200) {
-        console.log("Uploaded!");
-      } else {
-        console.warn("Error " + request.status + " occurred when trying to upload your file.");
-      }
-    };
-    request.open("POST", "http://foo.com/submitform.php");
-    request.send(formData);
-  }
-
   render() {
-    const {pixels, color, rows, columns, setColorValue} = this.props;
+    const {pixelClick, setColorValue} = this.props;
+    const {pixels, color, rows, columns} = this.props.pixelEditor;
+    const {host, protocol} = this.props.settings;
 
     const width = window.innerWidth > window.innerHeight
                   ? '56%'
@@ -101,19 +69,36 @@ export class PixelEditorView extends Component {
 
     return (
       <div className='pixelEditor container'>
-      <input type="button" onClick={this.handleUploadClick.bind(this)} value="send to MS3000" />
         <ul className='pixelList list' style={styles.ul}>
-          {pixels && pixels.map(this.renderPixel)}
+          {pixels && pixels.map(pixel => {
+            const {row, column, color} = pixel;
+            return (
+              <Pixel
+                key={`${column}-${row}`}
+                row={row}
+                column={column}
+                columns={columns}
+                color={color}
+                pixelClick={pixelClick}
+              />
+            );
+          })}
         </ul>
+
         <div className='colorPicker'>
           <RGBAInput
             color={color}
             setColorValue={setColorValue}
           />
-          <ImageInput
-            label='upload image'
-            rows={rows}
-            columns={columns}
+
+          <ImageInput label='upload image' />
+
+          <FileUploadInput
+            pixels={pixels}
+            height={rows}
+            width={columns}
+            url={[protocol, host].join('://')}
+            text='send to MS3000'
           />
         </div>
       </div>
