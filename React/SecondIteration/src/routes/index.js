1142cf70577afbe8cb5e63ab916ff69e58e76fe1
Jascha Ehrenreich <jascha@jaeh.at>
2016-01-10 20:59:44 +0100

    update routes

--- React/SecondIteration/src/routes/index.js
+++ React/SecondIteration/src/routes/index.js
@@ -7,14 +7,14 @@ import { Route, IndexRoute, Redirect } from 'react-router';
 // very easy to navigate to files regardless of how deeply nested
 // your current file is.
 import CoreLayout from 'layouts/CoreLayout';
-import HomeView from 'views/HomeView/HomeView';
+import AboutView from 'views/AboutView/AboutView';
 import PixelEditorView from 'views/PixelEditorView/PixelEditorView';
 import NotFoundView from 'views/NotFoundView/NotFoundView';
 
 export default (
   <Route path='/' component={CoreLayout}>
-    <IndexRoute component={HomeView} />
-    <Route path='/pixels' component={PixelEditorView} />
+    <IndexRoute component={PixelEditorView} />
+    <Route path='/pixels' component={AboutView} />
     <Route path='/404' component={NotFoundView} />
     <Redirect from='*' to='/404' />
   </Route>
