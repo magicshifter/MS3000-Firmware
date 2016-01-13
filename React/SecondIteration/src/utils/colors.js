8163c777e45ff5b3f2cfb317609980b0e2aef6f0
Jascha Ehrenreich <jascha@jaeh.at>
2016-01-13 17:13:48 +0100

    add rgba_fromArray, add error to rgba_toString

--- React/SecondIteration/src/utils/colors.js
+++ React/SecondIteration/src/utils/colors.js
@@ -1,5 +1,34 @@
+import {isRGBAObject} from 'utils/types';
 
 export function rgba_toString(color) {
+  if (!isRGBAObject(color)) {
+    console.error('invalid color passed to utils/colors.js -> rgba_toString', color);
+    return color;
+  }
   const {r, g, b, a} = color;
   return `rgba(${r}, ${g}, ${b}, ${a})`;
 }
+
+export const rgba_fromArray =
+  pixelArray => {
+    let convertedColors = [];
+    let currentColorId = 0;
+    const colorNames = ['r', 'b', 'g', 'a'];
+
+    for (let currentColorId = 0; currentColorId < pixelArray.length; currentColorId += 4) {
+      const currentColorName = colorNames[currentColorId];
+
+      let color = {};
+
+      Object.keys(colorNames)
+        .forEach(key => {
+          const colorName = colorNames[key];
+          const colorKey = currentColorId + key;
+          color[colorName] = pixelArray[colorKey];
+        });
+
+      convertedColors.push(color);
+    }
+
+    return convertedColors;
+  };
