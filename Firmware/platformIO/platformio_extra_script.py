b368e920bf0515c43b4bbfbd9782313ab6004edf
wizard23 <wizards23@gmail.com>
2016-01-14 19:26:32 +0100

    added extra baudrates as comments

--- Firmware/platformIO/platformio_extra_script.py
+++ Firmware/platformIO/platformio_extra_script.py
@@ -3,7 +3,7 @@ from SCons.Script import DefaultEnvironment
 env = DefaultEnvironment()
 
 env.Replace(
-   UPLOAD_SPEED='921600'
+    UPLOAD_SPEED='921600'
 #   UPLOAD_SPEED='460800'
 #   UPLOAD_SPEED='115200'
 )
