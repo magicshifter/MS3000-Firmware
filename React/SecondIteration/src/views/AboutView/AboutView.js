0315464ea17c528bdb5ecb6c1cfe658f1877d5df
wizard23 <wizards23@gmail.com>
2016-01-15 01:26:09 +0100

    added link to future firmware update page

--- React/SecondIteration/src/views/AboutView/AboutView.js
+++ React/SecondIteration/src/views/AboutView/AboutView.js
@@ -21,10 +21,21 @@ export class AboutView extends Component {
 
   render() {
     const {count, increment, decrement} = this.props;
+    const updateUrl = 'http://magicshifter.net/ms3000/update';
 
     return (
       <div className='container text-center'>
-        <h1>Welcome to the MagicShifter 3000 UI</h1>
+        <h1>Welcome to the MagicShifter 3000 Web Interface!</h1>
+        <br/>
+        Although we did our best to make this first beta firmware as good as possible we also know that it is far from being finished. 
+        The new MS3000 hardware allows so many new use cases for connected apps that it will take us some time to implement our vision of the perfect MS3000 user interface one feature at a time.
+        Expect the first firmware update in February 2016. When it's done you can always find the latest firmware (and instructions on how to update) at: <a href={updateUrl}>{updateUrl}</a>
+
+        <br/>
+        <br/>
+        Greetings
+        <br />The MagicShifter Team!
+      {/*
         <h2>
           Sample Counter:&nbsp;
           <span className={classes['counter--green']}>{count}</span>
@@ -42,7 +53,9 @@ export class AboutView extends Component {
         </button>
         <hr />
         <Link to='/404'>Go to 404 Page</Link>
+      {*/}
       </div>
+
     );
   }
 }
