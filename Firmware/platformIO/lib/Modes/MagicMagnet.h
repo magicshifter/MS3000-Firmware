2eaf1081f48d8c0e5cad3bb0b2fa6b6335736cda
Jay Vaughan <seclorum@mac.com>
2016-02-04 11:37:43 +0100

    red/green bars in compass mode

--- Firmware/platformIO/lib/Modes/MagicMagnet.h
+++ Firmware/platformIO/lib/Modes/MagicMagnet.h
@@ -1,6 +1,10 @@
 #ifndef _MAGNETOMETER_MODE_H
 #define _MAGNETOMETER_MODE_H
 
+//
+// render the magnetometer sensor data
+// 
+
 class MagicMagnetMode : public MagicShifterBaseMode 
 {
 private:
@@ -25,10 +29,10 @@ private:
       msSystem.msSensor.readMagnetometerData(msGlobals.ggMagnet);
 
       if ((msSystem.msSensor.readRegister(0x5E) & 0x02)) {
-        msSystem.logln("Magnetic Jamming detected");
+        msSystem.logln("magnetometer:go!");
         autoCalResetCounter++;
         if (autoCalResetCounter > 10) {
-          msSystem.logln("Resetting Hard Iron Estimation...");
+          msSystem.logln("magnetometer:reset");
           // M_CTRL_REG2: Hybrid auto increment, Magnetic measurement min/max detection function reset
           msSystem.msSensor.writeRegister(0x5C, 0x24);
           autoCalResetCounter = 0;
@@ -48,6 +52,12 @@ private:
       msSystem.msLEDs.fillPixels(0,0,0);
       int lednr = map(abs(degrees), 0, 180, 0, 15);
       msSystem.msLEDs.setPixels(lednr, 0, 0, 255, msGlobals.ggBrightness);
+
+      for (int lC=0;lC<lednr;lC++) 
+        msSystem.msLEDs.setPixels(lC, 0, 255, 0, msGlobals.ggBrightness / 2); // !J! hack
+
+      for (int lC=lednr+1;lC<MAX_LEDS;lC++) 
+        msSystem.msLEDs.setPixels(lC, 255, 0, 0, msGlobals.ggBrightness / 2); // !J! hack
 
       // msSystem.log("LED: "); msSystem.logln(String(lednr));
 
