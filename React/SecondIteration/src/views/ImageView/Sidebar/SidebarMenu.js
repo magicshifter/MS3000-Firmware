4b90e2ed1da509385d173f92141a6281c5c4c02a
Jascha Ehrenreich <jascha@jaeh.at>
2016-02-07 21:20:50 +0100

    add FloatingButtons to SidebarMenu

--- React/SecondIteration/src/views/ImageView/Sidebar/SidebarMenu.js
+++ React/SecondIteration/src/views/ImageView/Sidebar/SidebarMenu.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import {Link} from 'react-router';
 
+import FloatingButton from 'components/inputs/FloatingButton';
+
 import classes from './SidebarMenu.scss';
 
 export const SidebarMenu =
   () => (
     <nav className={classes['container']}>
       <ul>
-        <li><Link to='/'>controls</Link></li>
-        <li><Link to='/paint/images'>images</Link></li>
-        <li><Link to='/paint/text'>text</Link></li>
-        <li><Link to='/paint/filters'>filters</Link></li>
-        <li><Link to='/paint/settings'>settings</Link></li>
+        <li><Link to='/'>
+          <FloatingButton text='controls' />
+        </Link></li>
+        <li><Link to='/paint/images'>
+          <FloatingButton text='images' />
+        </Link></li>
+        <li><Link to='/paint/text'>
+          <FloatingButton text='text' />
+        </Link></li>
+        <li><Link to='/paint/filters'>
+          <FloatingButton text='filters' />
+        </Link></li>
+        <li><Link to='/paint/settings'>
+          <FloatingButton text='settings' />
+        </Link></li>
       </ul>
     </nav>
   );
