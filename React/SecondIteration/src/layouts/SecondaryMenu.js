da2d199ba8e77810a48a05397071e3177056cbd5
Jascha Ehrenreich <jascha@jaeh.at>
2016-02-08 19:26:43 +0100

    add SecondaryMenu

---
+++ React/SecondIteration/src/layouts/SecondaryMenu.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+// XXX import FileUploadInput from 'components/inputs/FileUploadInput';
+
+import classes from './SecondaryMenu.scss';
+
+import iconClasses from 'styles/icons.scss';
+
+export const SecondaryMenu =
+  () => (
+    <nav className={classes['container']}>
+      <ul>
+        <li>
+          <Link to='/paint/images'>
+            <i
+              className={`${iconClasses['icon']} ${iconClasses['image']}`}
+            />
+            <span>images</span>
+          </Link>
+        </li>
+        <li>
+          <Link to='/paint/font'>
+            <i
+              className={`${iconClasses['icon']} ${iconClasses['font']}`}
+            />
+            <span>font</span>
+          </Link>
+        </li>
+        <li>
+          <Link to='/paint/effects'>
+            <i
+              className={`${iconClasses['icon']} ${iconClasses['effects']}`}
+            />
+            <span>effects</span>
+          </Link>
+        </li>
+        <li>
+          <Link to='/paint/color'>
+            <i
+              className={`${iconClasses['icon']} ${iconClasses['color']}`}
+            />
+            <span>color</span>
+          </Link>
+        </li>
+        {/*
+        <li>
+            <FileUploadInput />
+        </li>
+        */}
+      </ul>
+    </nav>
+  );
+
+export default SecondaryMenu;
