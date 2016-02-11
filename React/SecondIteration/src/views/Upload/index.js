88d35aa934e3a0d157b4191175460e1fda38592d
Jascha Ehrenreich <jascha@jaeh.at>
2016-02-11 18:35:30 +0100

    start working on upload view

--- React/SecondIteration/src/views/Upload/index.js
+++ React/SecondIteration/src/views/Upload/index.js
@@ -3,14 +3,14 @@ import {connect} from 'react-redux';
 
 import {actions} from 'redux/modules/views/image';
 
-import ImageInput from 'components/inputs/ImageInput';
+import FileUploadInput from 'components/inputs/FileUploadInput';
 
 import classes from './Upload.scss';
 
 const mapStateToProps =
   () => ({});
 
-export class Images extends Component {
+export class Upload extends Component {
   static propTypes = {};
 
   render() {
@@ -19,10 +19,10 @@ export class Images extends Component {
         className={classes['container']}
       >
         <h5>upload</h5>
-        <ImageInput />
+        <FileUploadInput />
       </div>
     );
   }
 }
 
-export default connect(mapStateToProps, actions)(Images);
+export default connect(mapStateToProps, actions)(Upload);
