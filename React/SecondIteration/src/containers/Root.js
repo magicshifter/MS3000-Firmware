569c338327a31764dc281ed38da32eefa94e73eb
Jascha Ehrenreich <jascha@jaeh.at>
2016-02-11 18:33:25 +0100

    remove devtools for now

--- React/SecondIteration/src/containers/Root.js
+++ React/SecondIteration/src/containers/Root.js
@@ -22,15 +22,15 @@ export default class Root extends Component {
     if (__DEBUG__) {
       if (__DEBUG_NEW_WINDOW__) {
         if (!window.devToolsExtension) {
-          require('../redux/utils/createDevToolsWindow').default(this.props.store);
+          // require('redux/utils/createDevToolsWindow').default(this.props.store);
         } else {
           window.devToolsExtension.open();
         }
       } else if (!window.devToolsExtension) {
-        const DevTools = require('containers/DevTools').default;
-        return (
-          <DevTools />
-        );
+        // const DevTools = require('containers/DevTools').default;
+        // return (
+          // <DevTools />
+        // );
       }
     }
   }
