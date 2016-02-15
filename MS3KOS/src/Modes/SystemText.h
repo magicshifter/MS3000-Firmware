bcb726a8b76f71c3b891141af559f0ead4f82ab7
wizard23 <wizard23@gmail.com>
2016-02-15 22:51:27 +0100

    fixed SysVaqls autoconnect, added version entry

--- MS3KOS/src/Modes/SystemText.h
+++ MS3KOS/src/Modes/SystemText.h
@@ -1,9 +1,13 @@
+	MSColor aRED = 		{ 0xff, 0x00, 0x00 };
+	MSColor aGREEN = 	{ 0x00, 0xff, 0x00 };
+	MSColor aBLUE = 	{ 0x00, 0x00, 0xff };
+	MSColor aWHITE = 	{ 0xff, 0xff, 0xff };
+
 
 class SystemTextMode:public MagicShifterBaseMode {
 
   private:
-	// MagicShifterImageAbstr *msImage;
-	POVShakeSync shakeSync;
+  	MagicPOVMode lPOVMode;
 	MagicShifterImageText msMagicShakeText;
 
 	bool correctBrightness = false;
@@ -13,10 +17,9 @@ class SystemTextMode:public MagicShifterBaseMode {
   public:
 	const char *modeName = "SystemText";
 
-	void setText(char *label, char *value) {
-		MSColor aRED = 		{ 0xff, 0x00, 0x00 };
-		MSColor aGREEN = 	{ 0x00, 0xff, 0x00 };
-		MSColor aBLUE = 	{ 0x00, 0x00, 0xff };
+
+	void setText(char *label, char *value, MSColor color) {
+		
 
 		Coordinate_s tPos;
 
@@ -28,109 +31,107 @@ class SystemTextMode:public MagicShifterBaseMode {
 		msMagicShakeText.plotTextString(label, msGlobals.tBitmap4x5, tPos);
 
 		tPos.y += msGlobals.tBitmap4x5.header.frameHeight;
-		msGlobals.tBitmap4x5.color = aRED;
+		msGlobals.tBitmap4x5.color = color;
 
 		msMagicShakeText.plotTextString(value, msGlobals.tBitmap4x5, tPos);
-		shakeSync.setFrames(msMagicShakeText.getWidth() * FRAME_MULTIPLY);
+		lPOVMode.setImage(&msMagicShakeText);
 	}
 
 	void start() {
-		setText((char *) "SYSTEM", (char *) "VALUES");
+		setText((char *) "SYSTEM", (char *) "VALUES", aBLUE);
 	}
 
 // stop the mode
 	void stop(void) {
-		shakeSync.setFrames(0);
+		lPOVMode.setImage(NULL);
 	}
 
 	enum {
 		STR_IP = 0,
 		STR_SOFTIP = 1,
 		STR_SSID = 2,
-		STR_WIFI = 3
+		STR_WIFI = 3,
+		STR_VERSION = 4
 	};
 
 // step through a frame of the mode 
 	bool step() {
+		bool needsTextUpdate = false;
 
 		// cycle through the texts ..
 		if (msSystem.msButtons.msBtnAHit) {
 			msSystem.msButtons.msBtnAHit = false;	// !J! todo: button callbacks
 			sysCursor++;
-			if (sysCursor > 3)
+			if (sysCursor > 4)
 				sysCursor = 0;
 			msSystem.slog("cursor:");
 			msSystem.slogln(String(sysCursor));
-
-			if (sysCursor == STR_IP) {
-				setText((char *) String("IP").c_str(),
-						(char *) WiFi.localIP().toString().c_str());
-			} else if (sysCursor == STR_SOFTIP) {
-				setText((char *) String("SOFTIP").c_str(),
-						(char *) WiFi.softAPIP().toString().c_str());
-			} else if (sysCursor == STR_SSID) {
-				setText((char *) String("SSID").c_str(),
-						(char *) WiFi.SSID().c_str());
-			} else if (sysCursor == STR_WIFI) {
-				if  (WiFi.status() == WL_DISCONNECTED)
-					setText((char *) String("WIFI").c_str(), (char *) String("OFF").c_str());
-				else
-					setText((char *) String("WIFI").c_str(), (char *) String("ON").c_str());
-			}
+			needsTextUpdate = true;
 		}
 
 		if (msSystem.msButtons.msBtnPwrHit) {
+			msSystem.slogln("btpwr");
 			if (sysCursor == STR_WIFI) { // WIFI
+				msSystem.slogln("wifi cursor!");
 				if (msGlobals.ggEnableWIFI)  {
 					WiFi.disconnect(true);
 					msGlobals.ggEnableWIFI = false;
+					msSystem.slogln("disconnected");
 				}
 				else {
+					msSystem.slogln("autoconnect");
 					msGlobals.ggEnableWIFI = true;
-					msWebServer.start();
+					AutoConnect();
 				}
 				msSystem.msButtons.msBtnPwrHit = false;
 			}
+			needsTextUpdate = true;
 		}
 
-		// check accelerometer
-		if (shakeSync.update(msGlobals.ggAccel[1])) {
-			int index = shakeSync.getFrameIndex();
-
-			if (index > 0) {
-				byte povData[RGB_BUFFER_SIZE];
-
-				if (shouldBlackenBackground) {	// paint a background ..
-					for (int i = 0; i < MAX_LEDS * 4; i += 4) {
-						povData[i] = 0xff;
-						povData[i + 1] = 0x00;
-						povData[i + 2] = 0x00;
-						povData[i + 3] = 0x00;
-					}
-				}
-				int frame_index = index / FRAME_MULTIPLY;
-
-				msMagicShakeText.getFrameData(frame_index, povData);
-
-				if (correctBrightness) {
-
-					msSystem.msLEDs.loadBufferShort(povData);
-					msSystem.msLEDs.updateLEDs();
-					//delayMicroseconds(POV_TIME_MICROSECONDS);
-					msSystem.msLEDs.loadBufferLong(povData);
-					msSystem.msLEDs.updateLEDs();
-				} else {
-					msSystem.msLEDs.loadBuffer(povData);
-					msSystem.msLEDs.updateLEDs();
-					delayMicroseconds(POV_TIME_MICROSECONDS);
-					msSystem.msLEDs.fastClear();
-				}
-			} else {
-				msSystem.msLEDs.fastClear();
-				yield();
+		if (needsTextUpdate) {
+			if (sysCursor == STR_VERSION) {
+				setText((char *) String("VERSION").c_str(),
+						MS3KOS_VERSION, aWHITE);
 			}
+			if (sysCursor == STR_IP) {
+				setText((char *) String("IP").c_str(),
+						(char *) WiFi.localIP().toString().c_str(), aWHITE);
+			} else if (sysCursor == STR_SOFTIP) {
+				setText((char *) String("SOFTIP").c_str(),
+						(char *) WiFi.softAPIP().toString().c_str(), aWHITE);
+			} else if (sysCursor == STR_SSID) {
+				setText((char *) String("SSID").c_str(),
+						(char *) WiFi.SSID().c_str(), aWHITE);
+			} else if (sysCursor == STR_WIFI) {
+				if  (!msGlobals.ggEnableWIFI)
+					setText((char *) String("WIFI").c_str(), (char *) String("OFF").c_str(), aRED);
+				else
+					setText((char *) String("WIFI").c_str(), (char *) String("ON").c_str(), aGREEN);
 
+				Coordinate_s tPos;
+				tPos.x = 20;
+				tPos.y = 0;
+				tPos.y += msGlobals.tBitmap4x5.header.frameHeight;
+				if  (WiFi.status() != WL_CONNECTED)
+				{
+					msGlobals.tBitmap4x5.color = aRED;
+					msMagicShakeText.plotTextString("N", msGlobals.tBitmap4x5, tPos);
+				}
+				else
+				{	
+					msGlobals.tBitmap4x5.color = aGREEN;
+					msMagicShakeText.plotTextString("Y", msGlobals.tBitmap4x5, tPos);
+				}
+				lPOVMode.setImage(&msMagicShakeText);
+			}
+		}
+
+		if (lPOVMode.step()) {
 			return true;
+		} else {
+			
+			delay(30);
+			return false;
 		}
 
 		return false;
