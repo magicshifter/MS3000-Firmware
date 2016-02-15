da6c57f98dc1345eb9a77930a9ab9adc29709a84
Jay Vaughan <seclorum@mac.com>
2016-02-15 18:52:25 +0100

    Cleanup tools

--- Tools/collectDump.sh
+++
@@ -1,7 +0,0 @@
-dumpname=firmware-0x0000.bin
-buildname=./build/WiFiMagicShifterOS.cpp.bin
-targetdir=./messedUp/$1
-
-mkdir $targetdir
-cp $dumpname $targetdir
-cp $buildname $targetdir
--- Tools/dumpFlashAuto.sh
+++
@@ -1,9 +0,0 @@
-
-dumpname=firmware-0x0000.bin
-buildname=./build/WiFiMagicShifterOS.cpp.bin
-
-./esptool.py --baud 460800 read_flash 0x00000 $(stat -c '%s' $buildname) $dumpname
-cmp --verbose $buildname $dumpname
-
-#./esptool.py read_flash 0x00000 0xFFF my_app.elf-0x00000.bin
-#./esptool.py read_flash 0x40000 0xFFFF my_app.elf-0x40000.bin
