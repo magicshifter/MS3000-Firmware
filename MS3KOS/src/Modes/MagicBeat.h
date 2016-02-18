b55daf2e82150e081caf9bc5517e12a7695f9190
wizard23 <wizards23@gmail.com>
2016-02-18 22:13:04 +0100

    refactored Fixecomma hack out of beat detection mode

--- MS3KOS/src/Modes/MagicBeat.h
+++ MS3KOS/src/Modes/MagicBeat.h
@@ -3,11 +3,10 @@ class MagicBeatMode : public MagicShifterBaseMode {
 private:
 
 	int frame = 0;
-	long avgZ = 0;
-	long xPos;
+	float avgZ = 0;
 	int colIdx = 1;
 	int sensitivity = 0;
-	int renderMode = 0;
+		int renderMode = 1;
 
 	// Color of our beat
 	uint8_t r, g, b;
@@ -25,63 +24,86 @@ public:
 	virtual void stop(void) {
 	}
 
-#define FIXED_SHIFT 8
+	const int axis = 2;
+	const float avgF = 1./32.0;
 
 	virtual bool step(void) {
+		float xPos;
 
-		avgZ *= 31;
-		avgZ += msGlobals.ggAccelCount[2];
-		if (avgZ >= 0) {
-			avgZ += 0 + 15;
-		} else {
-			avgZ -= 0 + 15;
-		}
-		avgZ /= 32;
+		avgZ = avgZ * (1 - avgF) + avgF * msGlobals.ggAccel[axis];
 
 		frame++;
 
-		if (renderMode == 0) {
-			xPos = 7 * (1 << FIXED_SHIFT) + (1 << (FIXED_SHIFT - 1)) + (((msGlobals.ggAccelCount[2] - avgZ) * 40) >> sensitivity);
-			if (xPos < 0)
-				xPos = 0;
-			if (xPos > 15 * (1 << FIXED_SHIFT))
-				xPos = 15 * (1 << FIXED_SHIFT);
+		// if (renderMode == 0) {
+		// 	xPos = 7 * (1 << FIXED_SHIFT) + (1 << (FIXED_SHIFT - 1)) + (((msGlobals.ggAccelCount[2] - avgZ) * 40) >> sensitivity);
+		// 	if (xPos < 0)
+		// 		xPos = 0;
+		// 	if (xPos > 15 * (1 << FIXED_SHIFT))
+		// 		xPos = 15 * (1 << FIXED_SHIFT);
 
-			int8_t xPosInt = xPos >> FIXED_SHIFT;			
-			uint8_t xPosRemainder = (xPos & (1 << FIXED_SHIFT) - 1);
+		// 	int8_t xPosInt = xPos >> FIXED_SHIFT;			
+		// 	uint8_t xPosRemainder = (xPos & (1 << FIXED_SHIFT) - 1);
+
+		// 	msSystem.msLEDs.fillLEDs(0, 0, 0);
+			
+		// 	msSystem.msLEDs.setLED(xPosInt + 1, (r * xPosRemainder) >> FIXED_SHIFT, (g * xPosRemainder) >> FIXED_SHIFT, (b * xPosRemainder) >> FIXED_SHIFT,
+		// 		msGlobals.ggBrightness);
+
+		// 	xPosRemainder = ((1 << FIXED_SHIFT) - 1) - xPosRemainder;			
+
+		// 	msSystem.msLEDs.setLED(xPosInt, (r * xPosRemainder) >> FIXED_SHIFT, (g * xPosRemainder) >> FIXED_SHIFT, (b * xPosRemainder) >> FIXED_SHIFT,
+		// 		msGlobals.ggBrightness);
+
+		// 	msSystem.msLEDs.updateLEDs();
+
+		//} else { // renderMode = 1
+			xPos = (msGlobals.ggAccel[axis] - avgZ);
+
+			xPos *= 1024 >> sensitivity;
+
+			if (renderMode == 0) {
+				if (xPos < 0)
+					xPos = -xPos;
+
+				xPos -= 5;
+				if (xPos > MAX_LEDS)
+					xPos = MAX_LEDS;
+			}
+			else {
+				xPos += 7.5;
+			}
 
 			msSystem.msLEDs.fillLEDs(0, 0, 0);
+
+			if (renderMode == 0) {
+				for (int i = 0; i < xPos; i++)
+				msSystem.msLEDs.setLED((MAX_LEDS-1) - i, r, g, b, msGlobals.ggBrightness);
+			}
+			else {
+				int xPosInt = (int)(xPos);
+				float xPosRem = 1 - (xPos - xPosInt);			
 			
-			msSystem.msLEDs.setLED(xPosInt + 1, (r * xPosRemainder) >> FIXED_SHIFT, (g * xPosRemainder) >> FIXED_SHIFT, (b * xPosRemainder) >> FIXED_SHIFT,
-				msGlobals.ggBrightness);
+				if (xPosInt >= 0 && xPosInt < MAX_LEDS) {
+					msSystem.msLEDs.setLED(xPosInt, 
+						(int)(r * xPosRem), 
+						(int)(g * xPosRem),
+						(int)(b * xPosRem),
+						msGlobals.ggBrightness);
+				}
 
-			xPosRemainder = ((1 << FIXED_SHIFT) - 1) - xPosRemainder;			
-
-			msSystem.msLEDs.setLED(xPosInt, (r * xPosRemainder) >> FIXED_SHIFT, (g * xPosRemainder) >> FIXED_SHIFT, (b * xPosRemainder) >> FIXED_SHIFT,
-				msGlobals.ggBrightness);
-
+				xPosInt++;
+				xPosRem = 1 - xPosRem;
+				if (xPosInt >= 0 && xPosInt < MAX_LEDS) {
+					msSystem.msLEDs.setLED(xPosInt, 
+							(int)(r * xPosRem), 
+							(int)(g * xPosRem),
+							(int)(b * xPosRem),
+							msGlobals.ggBrightness);
+				}
+			}
+			
 			msSystem.msLEDs.updateLEDs();
-
-		} else { // renderMode = 1
-			xPos = (msGlobals.ggAccelCount[2] - avgZ);
-			
-			if (xPos < 0)
-				xPos = -xPos;
-
-			xPos >>= sensitivity;
-			
-			xPos -= 5;
-
-			if (xPos > 16)
-				xPos = 16;
-
-			msSystem.msLEDs.fillLEDs(0, 0, 0);
-
-			for (int i = 0; i < xPos; i++)
-				msSystem.msLEDs.setLED(15 - i, r, g, b, msGlobals.ggBrightness);
-
-			msSystem.msLEDs.updateLEDs();
-		}
+		//}
 
 		if (msSystem.msButtons.msBtnALongHit) {
 			renderMode++;
