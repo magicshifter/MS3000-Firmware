41d3836ae86f545361e5729dbbc90b780af35d0c
Jay Vaughan <seclorum@mac.com>
2016-03-07 21:27:11 +0100

    Firmware version is hand-coded for now: todo: automate

--- Tools/readFirmware.sh
+++ Tools/readFirmware.sh
@@ -5,7 +5,7 @@
 # the readout has started then you can release it
 # baudrates: 115200 460800 921600
 
-firmwareName=MS3000_Firmware_0.9.1.bin.2
+firmwareName=MS3000_Firmware_0.9.1.bin
 
 ./esptool/esptool.py --port $1 --baud 921600 read_flash 0x00000 0x400000 $firmwareName 
 
