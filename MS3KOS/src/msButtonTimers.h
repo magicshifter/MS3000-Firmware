7d44ad7e1a6a2364779bef9353b93a763cfd1f3e
Jay Vaughan <seclorum@mac.com>
2016-10-23 19:01:01 +0200

    funky_case goesCamel

--- MS3KOS/src/msButtonTimers.h
+++ MS3KOS/src/msButtonTimers.h
@@ -1,10 +1,26 @@
+//
+// Due to hardware design, the MS3000 power button is best read
+// on a timer, outside the scope of the rest of the system/OS/etc.
+//
+// Thus, we have a few things related to the read of the power
+// button in this module.
+//
 
+// The timer is polled frequently:
 os_timer_t aPowerButtonTimer;
+// .. ~8 times/second
 #define POWER_BUTTON_TIMER_PERIOD 125
 
-// !J! TODO: Adjust this:
+// !J! TODO: Adjust this according to feel:
 #define MIN_POWER_LEVEL_THRESHOLD (3.0f)
 
+//
+// The ESP8266 Timer-handler is plain 'ol C .. (in lieu of a class)
+// 
+
+//
+// Calculate the current voltage of the power system.
+// 
 float calculateVoltage(int adValue, int batVoltCalibration)
 {
 	float voltage;
@@ -20,6 +36,10 @@ float calculateVoltage(int adValue, int batVoltCalibration)
 	return voltage;
 }
 
+//
+// Handle the regular polling of the power button hardware and
+// make the results available to the rest of the system.
+//
 void PowerButtonTimerCallback(void *pArg) {
 	os_intr_lock();
 	// tickOccured = true;
@@ -32,6 +52,9 @@ void PowerButtonTimerCallback(void *pArg) {
 	os_intr_unlock();
 } // End of timerCallback
 
+//
+// Initialize the timers for use, and fire them off ..
+//
 void initMS3000SystemTimers()
 {
 	os_timer_setfn(&aPowerButtonTimer, PowerButtonTimerCallback, NULL);
