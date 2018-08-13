a8e70a72eb0221a10202051dfdc47859a7e3172f
Jay Vaughan <jay.vaughan@openresearch.com>
2018-08-14 00:06:01 +0200

    output RGB

--- MS3KOS/src/WebServer/WebServerAPI.h
+++ MS3KOS/src/WebServer/WebServerAPI.h
@@ -430,7 +430,7 @@ void handleGETAPSettings(void)
 }
 
 
-#define PROTOBUF_LEN 384
+#define PROTOBUF_LEN 1024
 
 
 bool ms3kModeLightNameFunc(pb_istream_t *stream, const pb_field_t *field, void **arg)
@@ -550,7 +550,12 @@ printf("handlePPBB64: xxxxx\n");
 
 		printf("handlePPBB64: submode is %d!\n", (int)msGlobals.protocolBuffer.modes.light.subMode);
 		// printf("handlePPBB64: submode name is %d!\n", strlen(msGlobals.protocolBuffer.modes.light.name));
-		printf("handlePPBB64: submode name is %s!\n", msGlobals.protocolBuffer.modes.light.name);
+		printf("handlePPBB64: networkName is %s!\n", msGlobals.protocolBuffer.networkName);
+		printf("handlePPBB64: beatMode is %d!\n", msGlobals.protocolBuffer.modes.beat.beatMode);
+		printf("handlePPBB64: sensitivity is %d!\n", msGlobals.protocolBuffer.modes.beat.sensitivity);
+		printf("handlePPBB64: R is %d!\n", msGlobals.protocolBuffer.modes.light.color.R);
+		printf("handlePPBB64: G is %d!\n", msGlobals.protocolBuffer.modes.light.color.G);
+		printf("handlePPBB64: B is %d!\n", msGlobals.protocolBuffer.modes.light.color.B);
 		printf("handlePPBB64: networkName is %s!\n", msGlobals.protocolBuffer.networkName);
 
 		msSystem.msESPServer.send(200, "text/plain", "OK");
