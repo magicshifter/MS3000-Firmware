da971e58fb3925f52fd2906a79f7ba45d4199caa
Jay Vaughan <jay.vaughan@openresearch.com>
2018-11-21 16:57:46 +0100

    LED

--- MS3KOS/src/Modes/MagicUpdate.h
+++ MS3KOS/src/Modes/MagicUpdate.h
@@ -14,61 +14,6 @@ public:
 	virtual void start() {
 		static int xx = 0;
 
-
-
-		ArduinoOTA.onStart([]() { 
-			// set LED's to start-state:
-			msSystem.slog(" update: start ");
-
-			// set a basic background to start with ..
-			for(int i=0;i<RGB_BUFFER_SIZE;i+=4) {
-				msGlobals.ggRGBLEDBuf[i] = msGlobals.ggBrightness | 0xe0;
-				msGlobals.ggRGBLEDBuf[i+1] = i * 2;
-				msGlobals.ggRGBLEDBuf[i+2] = 32 - i * 2;
-				msGlobals.ggRGBLEDBuf[i+3] = 32;
-			}
-	
-			msSystem.msLEDs.loadBuffer(msGlobals.ggRGBLEDBuf);
-			msSystem.msLEDs.updateLEDs();
-
-		});
-
-		ArduinoOTA.onProgress([](int total, int size) {
-			msSystem.slog(" update: .. ");
-			// !J! TODO: render progress
-			msSystem.msLEDs.setLED((xx + 0 * 3) & 0xF, 255, 0, 255, msGlobals.ggBrightness);
-
-			msSystem.msLEDs.loadBuffer(msGlobals.ggRGBLEDBuf);
-			msSystem.msLEDs.updateLEDs();
-
-		});
-
-		ArduinoOTA.onEnd([]() { 
-			// set LED's to end-state:
-			msSystem.slog(" update: end");
-			msSystem.msLEDs.setLED((xx + 0 * 3) & 0xF, 255, 255, 255, msGlobals.ggBrightness);
-			msSystem.msLEDs.loadBuffer(msGlobals.ggRGBLEDBuf);
-			msSystem.msLEDs.updateLEDs();
-
-		});
-
-		ArduinoOTA.onError([](ota_error_t error) { 
-			msSystem.slog(" update: error");
-
-			// set LED's to error-state:
-			for(int i=0;i<RGB_BUFFER_SIZE;i+=4) {
-				msGlobals.ggRGBLEDBuf[i] = msGlobals.ggBrightness | 0xe0;
-				msGlobals.ggRGBLEDBuf[i+1] = 255;
-				msGlobals.ggRGBLEDBuf[i+2] = 0;
-				msGlobals.ggRGBLEDBuf[i+3] = 0;
-			}
-			msSystem.msLEDs.loadBuffer(msGlobals.ggRGBLEDBuf);
-			msSystem.msLEDs.updateLEDs();
-
-		});
-
-		ArduinoOTA.begin();
-
 		// set a basic background to start with ..
 		for(int i=0;i<RGB_BUFFER_SIZE;i+=4) {
 			msGlobals.ggRGBLEDBuf[i] = msGlobals.ggBrightness | 0xe0;
@@ -79,6 +24,38 @@ public:
 
 		msSystem.msLEDs.loadBuffer(msGlobals.ggRGBLEDBuf);
 		msSystem.msLEDs.updateLEDs();
+
+
+		ArduinoOTA.onStart([]() { 
+			// set LED's to start-state:
+			msSystem.slog(" update: start ");
+			msSystem.msLEDs.fillLEDs(0, 64, 0, msGlobals.ggBrightness);
+			msSystem.msLEDs.updateLEDs();
+		});
+
+		ArduinoOTA.onProgress([](int total, int size) {
+			msSystem.slog(" update: .. ");
+			// !J! TODO: render progress
+			msSystem.msLEDs.fillLEDs(64, 0, 64, msGlobals.ggBrightness);
+			msSystem.msLEDs.setLED(( total / size ) & 0xf, 64, 64, 0, msGlobals.ggBrightness);
+			msSystem.msLEDs.updateLEDs();
+		});
+
+		ArduinoOTA.onEnd([]() { 
+			// set LED's to end-state:
+			msSystem.slog(" update: end");
+			msSystem.msLEDs.fillLEDs(128, 32, 64, msGlobals.ggBrightness);
+			msSystem.msLEDs.updateLEDs();
+		});
+
+		ArduinoOTA.onError([](ota_error_t error) { 
+			msSystem.slog(" update: error");
+			msSystem.msLEDs.fillLEDs(255, 0, 0, msGlobals.ggBrightness);
+			msSystem.msLEDs.updateLEDs();
+		});
+
+
+		ArduinoOTA.begin();
 
 		msSystem.slog(" update: start (mode) ");
 
