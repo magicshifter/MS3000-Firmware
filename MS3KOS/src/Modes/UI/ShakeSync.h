c3ff1f6d34f7c92c1808689534bdb600f9288735
Jay Vaughan <jay.vaughan@openresearch.com>
2018-11-28 17:55:58 +0100

    shakefync debug setup

--- MS3KOS/src/Modes/UI/ShakeSync.h
+++ MS3KOS/src/Modes/UI/ShakeSync.h
@@ -61,8 +61,10 @@ class POVShakeSync {
 		int micros;				// absolute
 	} ShakePoint;
 
-	const float hysteresis = 0.4;
-	const float sensitivity = 5.0;
+	const float hysteresis = 0;//0.4;
+
+	// sensitivity is the min distance between min and max to start poving
+	const float sensitivity = 5.;
 
 	// last time it took to go from acceleration minimum to maximum/ from max to min
 	int min2maxDelta, max2minDelta;
@@ -94,7 +96,7 @@ class POVShakeSync {
 	bool isFrameIndexActive = false;
 
   public:
-	// sensitivity is the min distance between min and max to start poving
+
 	 POVShakeSync(void) {
 	} void setFrames(int w) {
 		frames = w;
